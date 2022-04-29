3c739065353d405419b572fc208066c625797ca1
LilPoppy <lilpoppy96@gmail.com>
2022-04-29 15:06:32 -0400

    add constructor for properties

--- src/main/java/com/cobnet/spring/boot/dto/DynamicPageFieldProperties.java
+++ src/main/java/com/cobnet/spring/boot/dto/DynamicPageFieldProperties.java
@@ -1,13 +1,22 @@
 package com.cobnet.spring.boot.dto;
 
+import com.cobnet.common.KeyValuePair;
 import com.cobnet.interfaces.connection.web.PageFieldProperties;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DynamicPageFieldProperties implements PageFieldProperties {
 
     private Map<String, Object> map;
+
+    public DynamicPageFieldProperties(Map.Entry<String, Object>... properties) {
+
+        this.map = Arrays.stream(properties).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
     @Override
     public Object set(String key, Object value) {
--- src/main/java/com/cobnet/spring/boot/dto/DynamicPageProperties.java
+++ src/main/java/com/cobnet/spring/boot/dto/DynamicPageProperties.java
@@ -2,12 +2,19 @@ package com.cobnet.spring.boot.dto;
 
 import com.cobnet.interfaces.connection.web.PageProperties;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class DynamicPageProperties implements PageProperties {
 
     private Map<String, Object> map;
+
+    public DynamicPageProperties(Map.Entry<String, Object>... properties) {
+
+        this.map = Arrays.stream(properties).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
 
     @Override
     public Object set(String key, Object value) {
