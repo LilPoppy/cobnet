3d7493b54676e8f6aed75794114af6ee05f3057a
LilPoppy <lilpoppy96@gmail.com>
2022-05-04 14:40:56 -0400

    add keyValuePair to StringUtils

---
+++ src/main/java/com/cobnet/common/StringUtils.java
@@ -0,0 +1,12 @@
+package com.cobnet.common;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class StringUtils {
+
+    public static Map.Entry<String, String> keyValuePair(String str, String open, String close) {
+
+        return new KeyValuePair<>(str.replaceAll("\\[.*?\\]", ""), org.apache.commons.lang.StringUtils.substringBetween(str, open, close));
+    }
+}
